e066cf6a21d9ae63145da6f6284c6a44aa6d3cd3
peal-26 <edilasio@live.com>
2023-04-30 02:59:10 +0100

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter,
-  Routes as RoutesDom,
-  Route,
-  HashRouter,
-} from "react-router-dom";
+import { BrowserRouter, Routes as RoutesDom, Route } from "react-router-dom";
 
 // Paginas
 import Home from "../pages/home";
@@ -15,21 +10,21 @@ export default function Routes() {
   return (
     <BrowserRouter>
       {/* <HashRouter> */}
-        <RoutesDom>
-          <Route
-            path={`${BASE_URL}/gerador-ficheiro-saf-t-ao/`}
-            element={<Home />}
-          />
-          <Route
-            path={`${BASE_URL}/documentacao-api`}
-            element={<DocumentacaoApi />}
-          />
-          <Route
-            path={`${BASE_URL}/documentacao-agt`}
-            element={<DocumentacaoAgt />}
-          />
-          <Route path="*" element={<Home />} />
-        </RoutesDom>
+      <RoutesDom>
+        <Route
+          path={`${BASE_URL}/gerador-ficheiro-saf-t-ao/`}
+          element={<Home />}
+        />
+        <Route
+          path={`${BASE_URL}/documentacao-api`}
+          element={<DocumentacaoApi />}
+        />
+        <Route
+          path={`${BASE_URL}/documentacao-agt`}
+          element={<DocumentacaoAgt />}
+        />
+        <Route path="*" element={<Home />} />
+      </RoutesDom>
       {/* </HashRouter> */}
     </BrowserRouter>
   );
